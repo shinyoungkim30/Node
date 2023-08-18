e765ece6718b356a342cc90b602041a9eab07638
김신영 <130364428+shinyoungkim30@users.noreply.github.com>
2023-08-18 17:25:23 +0900

    commit

--- Chap6/learn-express/app.js
+++ Chap6/learn-express/app.js
@@ -4,10 +4,20 @@ const cookieParser = require('cookie-parser')
 const session = require('express-session')
 const dotenv = require('dotenv')
 const path = require('path')
+const nunjucks = require('nunjucks')
 
 dotenv.config()
+const indexRouter = require('./routes')
+const userRouter = require('./routes/user')
 const app = express()
 app.set('port', process.env.PORT || 3000)
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'html')
+
+nunjucks.configure('views', {
+    express: app,
+    watch: true,
+})
 
 app.use(morgan('dev'))
 app.use('/', express.static(path.join(__dirname, 'public')))
@@ -25,21 +35,20 @@ app.use(session({
     name: 'session-cookie'
 }))
 
-app.use((req, res, next) => {
-    console.log('모든 요청에 다 실행됩니다.');
-    next()
-})
+app.use('/', indexRouter)
+app.use('/user', userRouter)
 
-app.get('/', (req, res, next) => {
-    console.log('GET / 요청에서만 실행됩니다.');
-    next()
-}, (req, res) => {
-    throw new Error('에러는 에러 처리 미들웨어로 갑니다.')
+app.use((req, res, next) => {
+    const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`)
+    error.status = 404
+    next(error)
 })
 
 app.use((err, req, res, next) => {
-    console.error(err);
-    res.status(500).send(err.message)
+    res.locals.message = err.message
+    res.locals.error = process.env.NODE_ENV !== 'production' ? err : {}
+    res.status(err.status || 500)
+    res.render('error')
 })
 
 app.listen(app.get('port'), () => {
