9206f692dae520956e48184c7e7f4acfe2e9548c
김신영 <130364428+shinyoungkim30@users.noreply.github.com>
2023-08-26 17:10:09 +0900

    commit

--- Chap12/gif-chat/routes/index.js
+++ Chap12/gif-chat/routes/index.js
@@ -22,9 +22,23 @@ router.delete('/room/:id', removeRoom)
 router.post('/room/:id/chat', sendChat)
 
 try {
-    
+    fs.readdirSync('uploads')
 } catch (err) {
     console.error('uploads 폴더가 없어 uploads 폴더를 생성합니다.');
+    fs.mkdirSync('uploads')
 }
+const upload = multer({
+    storage: multer.diskStorage({
+        destination(req, file, done) {
+            done(null, 'uploads/')
+        },
+        filename(req, file, dont) {
+            const ext = path.extname(file.originalname)
+            done(null, path.basename(file.originalname, ext) + Date.now() + ext)
+        },
+    }),
+    limits: { fileSize: 5 * 1024 * 1024 },
+})
+router.post('/room/:id/gif', upload.single('gif'), sendGif)
 
 module.exports = router
--- Chap13/node-auction/app.js
+++ Chap13/node-auction/app.js
@@ -3,23 +3,35 @@ const path = require('path')
 const morgan = require('morgan')
 const cookieParser = require('cookie-parser')
 const session = require('express-session')
+const passport = require('passport')
 const nunjucks = require('nunjucks')
 const dotenv = require('dotenv')
-const ColorHash = require('color-hash').default
 
 dotenv.config()
-const webSocket = require('./socket')
 const indexRouter = require('./routes')
-const connect = require('./schemas')
+const authRouter = require('./routes/auth')
+const { sequelize } = require('./models')
+const passportConfig = require('./passport')
+const sse = require('./sse')
+const webSocket = require('./socket')
+const checkAuction = require('./checkAuction')
 
 const app = express()
-app.set('port', process.env.PORT || 8005)
+passportConfig()
+checkAuction()
+app.set('port', process.env.PORT || 8010)
 app.set('view engine', 'html')
 nunjucks.configure('views', {
     express: app,
     watch: true,
 })
-connect()
+sequelize.sync({ force: false })
+.then(() => {
+    console.log('데이터베이스 연결 성공');
+})
+.catch((err) => {
+    console.error(err);
+})
 
 const sessionMiddleware = session({
     resave: false,
@@ -28,36 +40,21 @@ const sessionMiddleware = session({
     cookie: {
         httpOnly: true,
         secure: false,
-    }
+    },
 })
 
 app.use(morgan('dev'))
 app.use(express.static(path.join(__dirname, 'public')))
-app.use('/gif', express.static(path.join(__dirname, 'uploads')))
+app.use('/img', express.static(path.join(__dirname, 'uploads')))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser(process.env.COOKIE_SECRET))
 app.use(sessionMiddleware)
-app.use(session({
-    resave: false,
-    saveUninitialized: false,
-    secret: process.env.COOKIE_SECRET,
-    cookie: {
-        httpOnly: true,
-        secure: false,
-    }
-}))
-
-app.use((req, res, next) => {
-    if (!req.session.color) {
-        const colorHash = new ColorHash()
-        req.session.color = colorHash.hex(req.sessionID)
-        console.log(req.session.color, req.sessionID);
-    }
-    next()
-})
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.use('/', indexRouter)
+app.use('/auth', authRouter)
 
 app.use((req, res, next) => {
     const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`)
@@ -67,7 +64,7 @@ app.use((req, res, next) => {
 
 app.use((err, req, res, next) => {
     res.locals.message = err.message
-    res.locals.error = process.env.NODE_NEW !== 'production' ? err : {}
+    res.locals.error = process.env.NODE_ENV !== 'production' ? err : {}
     res.status(err.status || 500)
     res.render('error')
 })
@@ -76,4 +73,5 @@ const server = app.listen(app.get('port'), () => {
     console.log(app.get('port'), '번 포트에서 대기 중');
 })
 
-webSocket(server, app, sessionMiddleware)
+webSocket(server, app)
+sse(server)
