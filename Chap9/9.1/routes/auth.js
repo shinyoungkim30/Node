72fb883ee8044c6d941d74a9d554f48386f2f75e
shinyoungkim30 <ksyq12@gmail.com>
2023-09-26 22:19:30 +0900

    commit

--- Chap9/9.1/routes/auth.js
+++ Chap9/9.1/routes/auth.js
@@ -15,4 +15,14 @@ router.post('/login', isNotLoggedIn, login);
 // GET /auth/logout
 router.get('/logout', isLoggedIn, logout);
 
+// GET /auth/kakao
+router.get('/kakao', passport.authenticate('kakao'));
+
+// GET /auth/kakao/callback
+router.get('/kakao/callback', passport.authenticate('kakao', {
+  failureRedirect: '/?error=카카오로그인 실패',
+}), (req, res) => {
+  res.redirect('/');
+});
+
 module.exports = router;
