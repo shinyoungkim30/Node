5f692853de4480c109e51df142541e8d06e397d2
김신영 <ksyq12@gmail.com>
2023-09-28 22:50:57 +0900

    commit

---
+++ Chap10/nodecat/controllers/index.js
@@ -0,0 +1,48 @@
+const axios = require('axios');
+
+const URL = process.env.API_URL;
+axios.defaults.headers.origin = process.env.ORIGIN;
+
+const request = async (req, api) => {
+  try {
+    if (!req.session.jwt) {
+      const tokenResult = await axios.post(`${URL}/token`, {
+        clientSecret: process.env.CLIENT_SECRET,
+      });
+      req.session.jwt = tokenResult.data.token;
+    }
+    return await axios.get(`${URL}${api}`, {
+      headers: { authorization: req.session.jwt },
+    });
+  } catch (error) {
+    if (error.response?.status === 419) {
+      delete req.session.jwt;
+      return request(req, api);
+    }
+    throw error;
+  }
+};
+
+exports.getMyPosts = async (req, res, next) => {
+  try {
+    const result = await request(req, '/posts/my');
+    res.json(result.data);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+};
+
+exports.searchByHashtag = async (req, res, next) => {
+  try {
+    const result = await request(
+      req, `/posts/hashtag/${encodeURIComponent(req.params.hashtag)}`,
+    );
+    res.json(result.data);
+  } catch (error) {
+    if (error.code) {
+      console.error(error);
+      next(error);
+    }
+  }
+};
