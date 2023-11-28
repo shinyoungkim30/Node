3ec509a038b03e949a4e9c339de3b9fc4189cc6c
김신영 <ksyq12@gmail.com>
2023-11-28 17:18:42 +0900

    event loop

--- section03/app.js
+++ section03/app.js
@@ -1,7 +1,10 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
   const url = req.url;
+  const method = req.method;
+
   if (url === '/') {
     res.write('<html>');
     res.write('<head><title>Enter Message</title></head>');
@@ -10,6 +13,23 @@ const server = http.createServer((req, res) => {
     );
     res.write('</html>');
     return res.end();
+  }
+  if (url === '/message' && method === 'POST') {
+    const body = [];
+    req.on('data', (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    return req.on('end', () => {
+      const parseBody = Buffer.concat(body).toString();
+      const message = parseBody.split('=')[1];
+
+      fs.writeFile('message.txt', message, (err) => {
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+      });
+    });
   }
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
