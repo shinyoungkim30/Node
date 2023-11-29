3673c45f21c025c7a6bf3709b292c0eaa3022bdb
김신영 <ksyq12@gmail.com>
2023-11-29 14:53:36 +0900

    router

--- section05/app.js
+++ section05/app.js
@@ -2,22 +2,16 @@ const express = require('express');
 
 const app = express();
 
+const adminRouter = require('./routes/admin');
+const shopRouter = require('./routes/shop');
+
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/add-product', (req, res, next) => {
-  console.log('In another middleware!');
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
-  );
-});
+app.use('/admin', adminRouter);
+app.use(shopRouter);
 
-app.post('/product', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-  res.send('<h1>Hello from Express!</h1>');
+app.use((req, res, next) => {
+  res.status(404).send('<h1>Page not found</h1>');
 });
 
 app.listen(3000);
