61a02a053bcb000decb5b989f7dccb7ffadac903
김신영 <ksyq12@gmail.com>
2023-11-29 21:12:19 +0900

    sendFile

--- section05/app.js
+++ section05/app.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 
 const app = express();
@@ -6,12 +8,13 @@ const adminRouter = require('./routes/admin');
 const shopRouter = require('./routes/shop');
 
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRouter);
 app.use(shopRouter);
 
 app.use((req, res, next) => {
-  res.status(404).send('<h1>Page not found</h1>');
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
