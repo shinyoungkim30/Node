d1673bfea098adc36080eb8f3669d04355a12543
김신영 <ksyq12@gmail.com>
2023-11-30 19:48:52 +0900

    controller

--- section07/app.js
+++ section07/app.js
@@ -2,22 +2,22 @@ const path = require('path');
 
 const express = require('express');
 
+const { get404Page } = require('./controllers/error');
+
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminData = require('./routes/admin');
+const adminRouter = require('./routes/admin');
 const shopRouter = require('./routes/shop');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRouter);
 app.use(shopRouter);
 
-app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
-});
+app.use(get404Page);
 
 app.listen(3000);
--- section07/routes/admin.js
+++ section07/routes/admin.js
@@ -1,25 +1,13 @@
 const express = require('express');
 
+const { getAddProduct, postAddProduct } = require('../controllers/product');
+
 const router = express.Router();
 
-const products = [];
-
 // /admin/add-product => GET
-router.get('/add-product', (req, res, next) => {
-  res.render('add-product', {
-    pageTitle: 'Add Product',
-    path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true,
-  });
-});
+router.get('/add-product', getAddProduct);
 
 // /admin/add-product => POST
-router.post('/add-product', (req, res, next) => {
-  products.push({ title: req.body.title });
-  res.redirect('/');
-});
+router.post('/add-product', postAddProduct);
 
-exports.routes = router;
-exports.products = products;
+module.exports = router;
